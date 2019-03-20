11da0ad4d8657fab48578f5844c78708a06dce08
zero <fll_005@163.com>
2019-03-20 20:25:26 +0800

    solve section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,24 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+//        Iterator it = collection1.iterator();
+//        List result = new ArrayList();
+//        while(it.hasNext()){
+//            String value = (String) it.next();
+//            if (collection2.get("value").contains(value)){
+//                result.add(value);
+//            }
+//        }
+        List<String> result = collection1.stream().filter(item -> collection2.get("value").contains(item)).collect(toList());
+        return result;
     }
 }
