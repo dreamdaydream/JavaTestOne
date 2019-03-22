924069bd5940150b8d2db6147d4c0cae831a29bd
zero <fll_005@163.com>
2019-03-22 08:27:17 +0800

    solve the section2

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,23 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> result = new HashMap<String,Integer>();
+        for(int i=0; i<collection1.size();i++){
+            String key = collection1.get(i);
+            if (result.containsKey(key)){
+                int count = result.get(key);
+                result.put(key,count+1);
+            }
+            else{
+                result.put(key,1);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,33 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> result = new HashMap<String,Integer>();
+        for(int i=0; i<collection1.size();i++){
+            String key = collection1.get(i);
+            int add = 1;
+            char keylast = key.charAt(0);
+            if (key.length()>1){
+                for(int j=0;j<key.length();j++) {
+                    if (key.charAt(j) >= 48 && key.charAt(j) <= 57) {
+                        add = Integer.valueOf(String.valueOf(key.charAt(j)));
+                    }
+                }
+            }
+            if (result.containsKey(String.valueOf(keylast))){
+                int count = result.get(String.valueOf(keylast));
+                result.put(String.valueOf(keylast),count+add);
+            }
+            else{
+                result.put(String.valueOf(keylast),add);
+            }
+        }
+        return result;
 
-        return null;
     }
 }
